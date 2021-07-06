ba8e6ab7e3d49c65265436db3750ab49d1f25b13
mhaiyang <mhy666@mail.dlut.edu.cn>
2021-07-06 09:51:20 +0800

    2021.07.06

---
+++ config.py
@@ -0,0 +1,22 @@
+"""
+ @Time    : 2021/7/6 09:46
+ @Author  : Haiyang Mei
+ @E-mail  : mhy666@mail.dlut.edu.cn
+ 
+ @Project : CVPR2021_PFNet
+ @File    : config.py
+ @Function: Configuration
+ 
+"""
+import os
+
+backbone_path = './backbone/resnet/resnet50-19c8e357.pth'
+
+datasets_root = '~/data/NEW'
+
+cod_training_root = os.path.join(datasets_root, 'train')
+
+chameleon_path = os.path.join(datasets_root, 'test/CHAMELEON')
+camo_path = os.path.join(datasets_root, 'test/CAMO')
+cod10k_path = os.path.join(datasets_root, 'test/COD10K')
+nc4k_path = os.path.join(datasets_root, 'test/NC4K')
